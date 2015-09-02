e79bed7482323abd5f94c8fdbae6483729b4672a
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2015-09-02 10:57:59 -0300

    Update set-domain to also update css files.

--- set-domain.sh
+++ set-domain.sh
@@ -3,7 +3,7 @@
 if [ -z $1 ]; then
   echo "Usage: ./set-domain.sh yourdomain. The string 'localhost:8080' will be replaced by yourdomain on all files."
 else
-  find . -type f -name "*.html" -exec sed -i "s/localhost:8080/$1/g" {} +
+  find . -type f \( -name "*.css" -o -name "*.html" \) -exec sed -i "s/localhost:8080/$1/g" {} +
 fi
 
 
